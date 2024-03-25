682d6935e691eeab8a4944f0dbf12e9ceb03dafe
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2024-03-25 19:46:50 +0900

    Part02/Chapter04 Done

--- Unreal5_Lecture_Project/Part2/ArenaBattle/Intermediate/Build/Win64/UnrealEditor/Inc/ArenaBattle/UHT/ArenaBattle.init.gen.cpp
+++ Unreal5_Lecture_Project/Part2/ArenaBattle/Intermediate/Build/Win64/UnrealEditor/Inc/ArenaBattle/UHT/ArenaBattle.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeArenaBattle_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xCA0A7B1C,
-				0x15AD3583,
+				0x05A22A32,
+				0xF4CFCE4C,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_ArenaBattle.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_ArenaBattle.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ArenaBattle(Z_Construct_UPackage__Script_ArenaBattle, TEXT("/Script/ArenaBattle"), Z_Registration_Info_UPackage__Script_ArenaBattle, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xCA0A7B1C, 0x15AD3583));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ArenaBattle(Z_Construct_UPackage__Script_ArenaBattle, TEXT("/Script/ArenaBattle"), Z_Registration_Info_UPackage__Script_ArenaBattle, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x05A22A32, 0xF4CFCE4C));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
