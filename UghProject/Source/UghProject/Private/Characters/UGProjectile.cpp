735ed8b916538b30e9a5ee064ac4d5931399fc97
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2024-06-04 18:46:50 +0900

    Feat: overlap

--- UghProject/Source/UghProject/Private/Characters/UGProjectile.cpp
+++ UghProject/Source/UghProject/Private/Characters/UGProjectile.cpp
@@ -23,6 +23,9 @@ AUGProjectile::AUGProjectile()
 
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 	Mesh->SetupAttachment(Collision);
+
+	Mesh->OnComponentBeginOverlap.AddDynamic(this, &AUGProjectile::OnMeshBeginOverlap);
+	Collision->OnComponentBeginOverlap.AddDynamic(this, &AUGProjectile::OnCollisionBeginOverlap);
 }
 
 
@@ -33,6 +36,16 @@ void AUGProjectile::BeginPlay()
 	float InLifeTime = Range / ProjectileMovement->InitialSpeed;
 
 	SetLifeSpan(InLifeTime);
+}
+
+void AUGProjectile::OnMeshBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
+
+void AUGProjectile::OnCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//UE_LOG(LogTemp, Warning, TEXT("AUGProjectile::OnSheredBeginOverlap"));
 }
 
 
--- UghProject/Source/UghProject/Public/Characters/UGProjectile.h
+++ UghProject/Source/UghProject/Public/Characters/UGProjectile.h
@@ -33,4 +33,10 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnMeshBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnCollisionBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
